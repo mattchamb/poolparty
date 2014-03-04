ce2cc8ff31e15f50048d927c9bf12111b9c62ec6
Matthew Chambers <matthew@chambers.net.nz>
2014-03-04 20:48:19 +1300

    Added a whole bunch of stuff, like getting it doing something.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3').verbose();
 var dbName = "./db.sqlite3";
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser())
+app.use(express.bodyParser());
 
 function getDbConn() {
     var db = new sqlite3.Database(dbName);
@@ -15,13 +15,11 @@ function getDbConn() {
 
 (function () {
     var db = getDbConn();
-    if (!fs.existsSync("./db.sqlite3")) {
-        db.serialize(function () {
-            db.run("CREATE TABLE IF NOT EXISTS Players (playerId INTEGER PRIMARY KEY, playerName TEXT)");
-            db.run("CREATE TABLE IF NOT EXISTS Match (matchId INTEGER PRIMARY KEY, winner INTEGER, loser INTEGER)");
-            db.close();
-        });
-    }
+    db.serialize(function() {
+        db.run("CREATE TABLE IF NOT EXISTS Players (playerId INTEGER PRIMARY KEY, playerName TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS Matches (matchId INTEGER PRIMARY KEY, winnerId INTEGER, loserId INTEGER, time TEXT)");
+        db.close();
+    });
 })();
 
 app.get("/api/players", function (req, res) {
@@ -33,7 +31,6 @@ app.get("/api/players", function (req, res) {
         db.close();
     });
 });
-
 
 app.post("/api/players", function (req, res) {
     var db = getDbConn();
@@ -59,7 +56,49 @@ app.delete("/api/players", function (req, res) {
     db.run("DELETE FROM Players WHERE playerId = $playerId", {
         $playerId: req.body.playerId
     }, function (err, ress) {
-        res.send(200);
+        if(err === null) {
+            res.send(200);            
+        } else {
+            res.send(500);
+        }
+        db.close();
+    });
+});
+
+app.get("/api/matches", function (req, res) {
+    var db = getDbConn();
+    db.all("SELECT * FROM Matches", function (err, rows) {
+        console.log(err);
+        res.json(200, {
+          matches: rows
+        });
+        db.close();
+    });
+});
+
+app.post("/api/matches", function (req, res) {
+    var db = getDbConn();
+    console.log(req.body);
+    db.run("INSERT INTO Matches VALUES (NULL, $winner, $loser, $time)", {
+            $winner: req.body.winnerId,
+            $loser: req.body.loserId,
+            $time: req.body.time
+        }, function (err) {
+            console.log(err);
+        if (err === null) {
+            res.json(200, {
+                inserted: {
+                    matchId: this.lastID,
+                    winnerId: req.body.winnerId,
+                    loserId: req.body.loserId,
+                    time: req.body.time
+                }
+            });
+        } else {
+            res.json(500, {
+                error: "Something happened :("
+            });
+        }
         db.close();
     });
 });
--- public/app.js
+++ public/app.js
@@ -1,55 +1,272 @@
+"use strict";
+///<reference path="~/public/vendor/knockout.debug.js"/>
+///<reference path="~/public/vendor/jquery.js"/>
+
+function dataAccess() {
+    var getAllPlayers = function(callback) {
+        $.ajax("/api/players", {
+            type: "GET",
+            contentType: "application/json",
+            success: function(result) {
+                var players = [];
+                for (var i = 0; i < result.players.length; i++) {
+                    var p = result.players[i];
+                    players.push({
+                        playerName: p.playerName,
+                        playerId: p.playerId
+                    });
+                }
+                callback(players);
+            }
+        });
+    };
+
+    var savePlayer = function(playerName, callback) {
+        $.ajax("/api/players", {
+            data: ko.toJSON({
+                playerName: playerName
+            }),
+            type: "POST",
+            contentType: "application/json",
+            success: function(result) {
+                callback({
+                    playerId: result.inserted.playerId,
+                    playerName: playerName
+                });
+            }
+        });
+    };
+
+    var deletePlayer = function(playerId, callback) {
+        $.ajax("/api/players", {
+            data: ko.toJSON({
+                playerId: playerId
+            }),
+            type: "DELETE",
+            contentType: "application/json",
+            success: function() {
+                callback();
+            }
+        });
+    };
+
+    var getAllMatches = function(callback) {
+        $.ajax("/api/matches", {
+            type: "GET",
+            contentType: "application/json",
+            success: function(result) {
+                var matches = [];
+                for (var i = 0; i < result.matches.length; i++) {
+                    var p = result.matches[i];
+                    matches.push({
+                        matchId: p.matchId,
+                        winnerId: p.winnerId,
+                        loserId: p.loserId,
+                        time: p.time,
+                    });
+                }
+                callback(matches);
+            }
+        });
+    };
+
+    var saveMatch = function(winnerId, loserId, callback) {
+        $.ajax("/api/matches", {
+            data: ko.toJSON({
+                winnerId: winnerId,
+                loserId: loserId,
+                time: new Date().toTimeString()
+            }),
+            type: "POST",
+            contentType: "application/json",
+            success: function(result) {
+                callback({
+                    playerId: result.inserted.matchId,
+                    winnerId: result.inserted.winnerId,
+                    loserId: result.inserted.loserId,
+                    time: result.inserted.time,
+                });
+            }
+        });
+    };
+
+    return {
+        getAllPlayers: getAllPlayers,
+        savePlayer: savePlayer,
+        deletePlayer: deletePlayer,
+        getAllMatches: getAllMatches,
+        saveMatch: saveMatch
+    };
+}
 
 function PoolPartyViewModel() {
     var self = this;
+    self.dal = dataAccess();
+    
+    self.tabs = [
+        {
+            id: "allPlayers",
+            text: "All Players"
+        },
+        {
+            id: "allMatches",
+            text: "All Matches"
+        },
+        {
+            id: "selectedPlayerStats",
+            text: "Selected Player Stats"
+        }];
 
     self.newPlayerName = ko.observable();
     self.players = ko.observableArray([]);
+    self.matches = ko.observableArray([]);
+    self.selectedWinner = ko.observable();
+    self.selectedLoser = ko.observable();
+    self.chosenTabId = ko.observable(self.tabs[0].id);
 
-    $.ajax("/api/players", {
-        type: "GET", contentType: "application/json",
-        success: function (result) {    
-            for (var i = 0; i < result.players.length; i++) {
-                var p = result.players[i];
-                self.players.push({
-                    playerName: p.playerName,
-                    playerId: p.playerId
-                });
-            }
+    self.goToTab = function(tab) {
+        self.chosenTabId(tab.id);
+    };
+
+    self.dal.getAllPlayers(function(players) {
+        for (var i = 0; i < players.length; i++) {
+            self.players.push(players[i]);
+        }
+        self.selectedWinner(self.players[0]);
+        self.selectedLoser(self.players[1]);
+    });
+    
+    self.dal.getAllMatches(function(matches) {
+        for (var i = 0; i < matches.length; i++) {
+            self.matches.push(matches[i]);
         }
     });
 
-    self.savePlayer = function () {
-        $.ajax("/api/players", {
-            data: ko.toJSON({
-                playerName: self.newPlayerName()
-            }),
-            type: "POST", contentType: "application/json",
-            success: function (result) {
-                self.players.push({
-                    playerId: result.playerId,
-                    playerName: self.newPlayerName()
-                });
+    self.selectedPlayer = ko.observable();
+
+    self.setSelectedPlayer = function(player) {
+
+        var timesWon = function() {
+            var result = 0;
+            for (var i = 0; i < self.matches().length; i++) {
+                var m = self.matches()[i];
+                if (m.winnerId == player.playerId) {
+                    result++;
+                }
             }
+            return result;
+        };
+        
+        var timesPlayed = function() {
+            var result = 0;
+            for (var i = 0; i < self.matches().length; i++) {
+                var m = self.matches()[i];
+                if (m.winnerId == player.playerId || m.loserId == player.playerId) {
+                    result++;
+                }
+            }
+            return result;
+        };
+
+        var selectedPlayerModel = {
+            playerId: player.playerId,
+            playerName: player.playerName,
+            playersWonAgainst: ko.computed(function() {
+                var losers = [];
+                for (var i = 0; i < this.matches().length; i++) {
+                    var m = this.matches()[i];
+                    if(m.winnerId == player.playerId && losers.indexOf(m.loserId) === -1) {
+                        losers.push(m.loserId);
+                    }
+                }
+                return losers;
+            }, self),
+            playersLostAgainst: ko.computed(function() {
+                var winners = [];
+                for (var i = 0; i < this.matches().length; i++) {
+                    var m = this.matches()[i];
+                    if(m.loserId == player.playerId && winners.indexOf(m.winnerId) === -1) {
+                        winners.push(m.winnerId);
+                    }
+                }
+                return winners;
+            }, self),
+            title: ko.computed(function () {
+                var totalMatches = timesPlayed();
+                if(totalMatches === 0) {
+                    return player.playerName + " is untested!";
+                }
+                var wins = timesWon();
+                var winPercentage = (wins / totalMatches * 100).toFixed(2);
+                return player.playerName + " has won " + wins + " matches out of " + totalMatches + " (" + winPercentage + "%)";
+            }, self)
+        };
+        self.selectedPlayer(selectedPlayerModel);
+    };
+
+    self.getSelectedPlayerId = function() {
+        if(self.selectedPlayer()) {
+            return self.selectedPlayer().playerId;
+        }
+        return null;
+    };
+
+    self.currentPanel = ko.observable("allMatches");
+
+    self.getWinsForPlayer = function(player) {
+        var wins = 0;
+        for (var i = 0; i < self.matches().length; i++) {
+            if(self.matches()[i].winnerId === player.playerId) {
+                wins++;
+            }
+        }
+        return wins;
+    };
+    
+    self.getLossesForPlayer = function(player) {
+        var losses = 0;
+        for (var i = 0; i < self.matches().length; i++) {
+            if(self.matches()[i].loserId === player.playerId) {
+                losses++;
+            }
+        }
+        return losses;
+    };
+
+    self.getPlayerNameById = function(playerId) {
+        for (var i = 0; i < self.players().length; i++) {
+            if(self.players()[i].playerId === playerId) {
+                return self.players()[i].playerName;
+            }
+        }
+        return "UNKNOWN PLAYER";
+    };
+
+    self.savePlayer = function () {
+        if(self.newPlayerName().length === 0) {
+            return;
+        }
+        self.dal.savePlayer(self.newPlayerName(), function(player) {
+            self.players.push(player);
+            self.newPlayerName("");
         });
     };
 
     self.deletePlayer = function (player) {
-        $.ajax("/api/players", {
-            data: ko.toJSON({
-                playerId: player.playerId
-            }),
-            type: "DELETE", contentType: "application/json",
-            success: function (result) {
-                self.players.destroy(player);
-            }
+        self.dal.deletePlayer(player.playerId, function() {
+            self.players.destroyAll(player);
         });
     };
 
-    self.recordVictory = function (asdf) {
-
+    self.recordVictory = function () {
+        if(self.selectedWinner().playerId === self.selectedLoser().playerId) {
+            alert("You can't play against yourself.");
+            return;
+        }
+        self.dal.saveMatch(self.selectedWinner().playerId, self.selectedLoser().playerId, function(match) {
+            self.matches.push(match);
+        });
     };
 }
-
-
 
 ko.applyBindings(new PoolPartyViewModel());
